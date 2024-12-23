e1928b66cda8711cd0c690a961e17c308adad501
reemhasan1234 <120267270+reemhasan1234@users.noreply.github.com>
2024-12-23 19:34:05 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:projectflutter/botton.dart';
-import 'package:projectflutter/f.dart';
-import 'package:projectflutter/fffffinal.dart';
-import 'package:projectflutter/final.dart';
-import 'package:projectflutter/fp.dart';
 import 'package:projectflutter/login.dart';
 import 'package:projectflutter/newfile.dart';
-import 'package:projectflutter/reem.dart';
-import 'package:projectflutter/stateful1.dart';
-import 'package:projectflutter/statefull.dart';
-import 'package:projectflutter/loginfood.dart';
 
 void main() {
   runApp(const MyApp());
